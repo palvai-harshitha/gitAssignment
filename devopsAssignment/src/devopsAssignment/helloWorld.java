9f4c29a59d072fa4e677ededd51f25c3087bc9ab
palvai-harshu <palvaiphr@gmail.com>
2024-09-02 17:37:11 +0530

    First Java Project Commit

---
+++ devopsAssignment/src/devopsAssignment/helloWorld.java
@@ -0,0 +1,8 @@
+package devopsAssignment;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!! This is my java project for Devops Assignment");
+	}
+}
